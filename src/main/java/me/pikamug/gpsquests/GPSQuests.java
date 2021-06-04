f4b6ca3800bbfbfe87f829b2f6e9f2324a290282
PikaMug <2267126+PikaMug@users.noreply.github.com>
2021-06-04 06:35:37 -0400

    New flexibility when updating target location. Bump version

--- src/main/java/me/pikamug/gpsquests/GPSQuests.java
+++ src/main/java/me/pikamug/gpsquests/GPSQuests.java
@@ -14,6 +14,7 @@ package me.pikamug.gpsquests;
 
 import java.util.ConcurrentModificationException;
 import java.util.LinkedList;
+import java.util.Map.Entry;
 
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -35,11 +36,14 @@ import me.blackvein.quests.Quest;
 import me.blackvein.quests.Quester;
 import me.blackvein.quests.Quests;
 import me.blackvein.quests.Stage;
+import me.blackvein.quests.enums.ObjectiveType;
 import me.blackvein.quests.events.quest.QuestQuitEvent;
+import me.blackvein.quests.events.quest.QuestUpdateCompassEvent;
 import me.blackvein.quests.events.quester.QuesterPostChangeStageEvent;
 import me.blackvein.quests.events.quester.QuesterPostCompleteQuestEvent;
 import me.blackvein.quests.events.quester.QuesterPostFailQuestEvent;
-import me.blackvein.quests.events.quester.QuesterPostStartQuestEvent;
+import me.blackvein.quests.events.quester.QuesterPostUpdateObjectiveEvent;
+import net.md_5.bungee.api.ChatColor;
 
 public class GPSQuests extends JavaPlugin {
     private GPSAPI gpsapi;
@@ -88,7 +92,7 @@ public class GPSQuests extends JavaPlugin {
         
         if (pm.getPlugin("GPS") != null) {
             if (!pm.getPlugin("GPS").isEnabled()) {
-                for (final Quester q : quests.getQuesters()) {
+                for (final Quester q : quests.getOnlineQuesters()) {
                     if (q != null) {
                         if (gpsapi.gpsIsActive(q.getPlayer())) {
                             gpsapi.stopGPS(q.getPlayer());
@@ -139,9 +143,20 @@ public class GPSQuests extends JavaPlugin {
     }
     
     private class QuestsListener implements Listener {
+        
         @EventHandler
-        public void onQuesterPostStartQuest(final QuesterPostStartQuestEvent event) {
+        public void onQuestUpdateCompass(final QuestUpdateCompassEvent event) {
             updateGPS(event.getQuest(), event.getQuester());
+        }
+        
+        @EventHandler
+        public void onQuesterPostUpdateObjective(final QuesterPostUpdateObjectiveEvent event) {
+            if (event.getObjective().getType() == ObjectiveType.TALK_TO_NPC
+                    || event.getObjective().getType() == ObjectiveType.KILL_NPC
+                    || event.getObjective().getType() == ObjectiveType.REACH_LOCATION
+                    || event.getObjective().getType() == ObjectiveType.DELIVER_ITEM) {
+                updateGPS(event.getQuest(), event.getQuester());
+            }
         }
         
         @EventHandler
@@ -177,9 +192,24 @@ public class GPSQuests extends JavaPlugin {
     public boolean updateGPS(final Quest quest, final Quester quester) {
         final Stage stage = quester.getCurrentStage(quest);
         if (stage == null) {
-            getLogger().severe("Called updateGPS() with a null stage from quest " + quest.getName());
             return false;
         }
+        
+        int stageIndex = 0;
+        for (final Entry<Quest, Integer> set : quester.getCurrentQuests().entrySet()) {
+            if (set.getKey().getId().equals(quest.getId())) {
+                stageIndex = set.getValue();
+            }
+        }
+        
+        int objectiveIndex = 0;
+        for (final String objective : quester.getCurrentObjectives(quest, false)) {
+            if (objective.startsWith(ChatColor.GREEN.toString())) {
+                break;
+            }
+            objectiveIndex++;
+        }
+        
         final LinkedList<Location> targetLocations = new LinkedList<Location>();
         if (citizensToInteract && stage.getCitizensToInteract() != null && stage.getCitizensToInteract().size() > 0) {
             if (quests.getDependencies().getCitizens() != null) {
@@ -203,30 +233,16 @@ public class GPSQuests extends JavaPlugin {
             }
         }
         if (targetLocations != null && !targetLocations.isEmpty()) {
-            int index = 1;
-            final String pointName = "quests-" + quester.getPlayer().getUniqueId().toString() + "-" + quest.getName() + "-" + stage.toString() + "-";
             final Player p = quester.getPlayer();
-            for (final Location l : targetLocations) {
-                if (l.getWorld().getName().equals(p.getWorld().getName())) {
-                    if (!gpsapi.gpsIsActive(p)) {
-                        try {
-                            gpsapi.addPoint(pointName + index, l);
-                        } catch (final IllegalArgumentException e) {
-                            // Player died or is re-taking quest
-                            gpsapi.startGPS(quester.getPlayer(), pointName + index);
-                        }
-                        index++;
-                    }
+            final String pointName = "quests-" + p.getUniqueId().toString() + "-" + quest.getId() + "-" + stageIndex + "-" + objectiveIndex;
+            try {
+                if (objectiveIndex < targetLocations.size()) {
+                    gpsapi.addPoint(pointName, targetLocations.get(objectiveIndex));
                 }
+            } catch (final IllegalArgumentException e) {
+                // Player may have died or is re-taking quest
             }
-            for (int i = 1 ; i < targetLocations.size(); i++) {
-                if (!gpsapi.gpsIsActive(quester.getPlayer())) {
-                    gpsapi.connect(pointName + i, pointName + (i + 1), true);
-                }
-            }
-            if (!gpsapi.gpsIsActive(quester.getPlayer())) {
-                gpsapi.startGPS(quester.getPlayer(), pointName + (index - 1));
-            }
+            gpsapi.startGPS(p, pointName);
         }
         return targetLocations != null && !targetLocations.isEmpty();
     }
@@ -245,7 +261,7 @@ public class GPSQuests extends JavaPlugin {
         if (gpsapi.gpsIsActive(p)) {
             gpsapi.stopGPS(p);
             for (final Point point : gpsapi.getAllPoints()) {
-                if (point.getName().startsWith("quests-" + p.getUniqueId().toString() + "-" + quest.getName())) {
+                if (point.getName().startsWith("quests-" + p.getUniqueId().toString() + "-" + quest.getId())) {
                     try {
                         gpsapi.removePoint(point.getName());
                     } catch (final ConcurrentModificationException e) {
